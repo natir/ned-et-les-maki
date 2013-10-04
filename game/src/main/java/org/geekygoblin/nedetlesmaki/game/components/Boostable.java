879e7ae0e6b33e3ee9b764d39015eafe3dc76c43
Pierre <pierre@marijon.fr>
2013-10-04 17:41:53 +0200

    Add boostable components, contain the numbre of case after why add boostable capacity

---
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/components/Boostable.java
@@ -0,0 +1,24 @@
+package org.geekygoblin.nedetlesmaki.game.components;
+
+import com.artemis.Component;
+
+/**
+ *
+ * @author natir
+ */
+public class Boostable extends Component {
+    
+    private int nbCase;
+
+    public Movable(int nbCase) {
+	this.nbCase = nbCase;
+    }
+
+    public int getNbCase() {
+	return this.nbCase;
+    }
+
+    public int setNbCase(int nbCase) {
+       this.nbCase = nbCase;
+    }
+}
