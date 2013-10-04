422178727debf560ef504a708760fb8640f73e85
Pierre <pierre@marijon.fr>
2013-10-04 16:35:54 +0200

    Add postion components.

---
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/components/Position.java
@@ -0,0 +1,33 @@
+package org.geekygoblin.nedetlesmaki.game.components;
+
+import com.artemis.Component;
+
+/**
+ *
+ * @author natir
+ */
+public class Position extends Component {
+    
+    private int x, y;
+
+    public Positon(int x, int y, String texture) {
+	this.x = x;
+	this.y = y;
+    }
+
+    public int getX() {
+	return x;
+    }
+
+    public int getY() {
+	return y;
+    }
+
+    public void setX(int x) {
+	this.x = x;
+    }
+
+    public void setY(int y) {
+	this.y = y;
+    }
+}
