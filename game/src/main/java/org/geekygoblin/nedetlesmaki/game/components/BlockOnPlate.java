3ae20ad1c6ac115e24dee30ec63e48a096145f1b
Pierre <pierre@marijon.fr>
2013-10-04 18:11:41 +0200

    block on plate, true if entity need block by plate.

---
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/components/BlockOnPlate.java
@@ -0,0 +1,24 @@
+package org.geekygoblin.nedetlesmaki.game.components;
+
+import com.artemis.Component;
+
+/**
+ *
+ * @author natir
+ */
+public class BlockOnPlate extends Component {
+    
+    private boolean block;
+
+    public BlockOnPlate(boolean block) {
+	this.block = block;
+    }
+
+    public boolean block() {
+	return this.block;
+    }
+
+    public void setBlock(boolean stop) {
+       this.block = block;
+    }
+}
