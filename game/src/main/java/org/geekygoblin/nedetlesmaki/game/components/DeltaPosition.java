31e517d47b6e71aa19b396f6a1b51771fe2c8f12
Pierre <pierre@marijon.fr>
2013-10-06 21:59:18 +0200

    Add components, for move an entity.

---
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/components/DeltaPosition.java
@@ -0,0 +1,33 @@
+package org.geekygoblin.nedetlesmaki.game.components;
+
+import com.artemis.Component;
+
+/**
+ *
+ * @author natir
+ */
+public class DeltaPosition extends Component {
+    
+    private int x, y;
+
+    public DeltaPosition(int x, int y, String texture) {
+	this.x = x;
+	this.y = y;
+    }
+
+    public int getX() {
+	return x;
+    }
+
+    public int getY() {
+	return y;
+    }
+
+    public void setX(int x) {
+	this.x = x;
+    }
+
+    public void setY(int y) {
+	this.y = y;
+    }
+}
