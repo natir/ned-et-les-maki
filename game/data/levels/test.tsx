9a16e1b0053df7c2da4d22a57b9f2a3ab886b66d
jymis <nwad@crypticgate.net>
2013-10-27 17:37:02 +0100

    add 4 levels, new sprites and update objects properties

--- game/data/levels/test.tsx
+++ game/data/levels/test.tsx
@@ -3,15 +3,25 @@
  <properties>
   <property name="debug" value="false"/>
  </properties>
- <image source="test.png" width="280" height="285"/>
+ <image source="test.png" width="280" height="342"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="ned"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="green_plate"/>
+  </properties>
+ </tile>
  <tile id="2">
   <properties>
-   <property name="type" value="maki"/>
+   <property name="type" value="green_maki_ok"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="type" value="maki_vert"/>
+   <property name="type" value="green_maki"/>
   </properties>
  </tile>
  <tile id="4">
@@ -26,12 +36,52 @@
  </tile>
  <tile id="6">
   <properties>
-   <property name="type" value="plateau_rouge"/>
+   <property name="type" value="pink_plate"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" value="pink_maki_ok"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="type" value="maki_rouge"/>
+   <property name="type" value="pink_maki"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="type" value="ned"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="type" value="ned"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="type" value="box"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="type" value="blue_plate"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="type" value="blue_maki_ok"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="type" value="blue_maki"/>
   </properties>
  </tile>
 </tileset>
