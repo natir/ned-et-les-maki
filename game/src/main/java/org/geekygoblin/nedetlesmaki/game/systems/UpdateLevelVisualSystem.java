fb0e41d84cae4c2736eb17ebd2c9597fde6f9eca
Pierre <pierre@marijon.fr>
2013-11-03 12:13:30 +0100

    Update the move of ned.

--- game/src/main/java/org/geekygoblin/nedetlesmaki/game/systems/UpdateLevelVisualSystem.java
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/systems/UpdateLevelVisualSystem.java
@@ -95,33 +95,29 @@ public class UpdateLevelVisualSystem extends VoidEntitySystem {
 	SpritePuppetControls updatable = new SpritePuppetControls(sprite);
 	
 	if(diff.getX() > 0) {
-	    System.out.print("Move up");
 	    updatable.startAnimation(anims.getAnimationByName("walk_up"))
-		.moveTo(new Vector3f(pos.x + 25.0f, pos.y - 10.0f, pos.z), 0.5f)
+		.moveTo(new Vector3f(pos.x + 28.0f, pos.y - 13.5f, pos.z), 0.5f)
 		.stopAnimation();
 	    e.addComponent(updatable);
 	    e.changedInWorld();
 	}
 	else if(diff.getX() < 0) {
-	    System.out.print("Move down");
 	    updatable.startAnimation(anims.getAnimationByName("walk_down"))
-		.moveTo(new Vector3f(pos.x - 25.0f, pos.y + 25, pos.z), 0.5f)
+		.moveTo(new Vector3f(pos.x - 28f, pos.y + 13.5f, pos.z), 0.5f)
         	.stopAnimation();
 	    e.addComponent(updatable);
 	    e.changedInWorld();
 	}
 	else if(diff.getY() > 0) {
-	    System.out.print("Move left");
 	    updatable.startAnimation(anims.getAnimationByName("walk_left"))
-		.moveTo(new Vector3f(pos.x - 25.0f, pos.y + 25, pos.z), 0.5f)
+		.moveTo(new Vector3f(pos.x - 28.0f, pos.y - 13.5f, pos.z), 0.5f)
 	        .stopAnimation();
 	    e.addComponent(updatable);
 	    e.changedInWorld();
 	}
 	else if(diff.getY() < 0) {
-	    System.out.print("Move right");
 	    updatable.startAnimation(anims.getAnimationByName("walk_right"))
-		.moveTo(new Vector3f(pos.x + 25.0f, pos.y - 25, pos.z), 0.5f)
+		.moveTo(new Vector3f(pos.x + 28.0f, pos.y + 13.5f, pos.z), 0.5f)
 		.stopAnimation();
 	    e.addComponent(updatable);
 	    e.changedInWorld();
