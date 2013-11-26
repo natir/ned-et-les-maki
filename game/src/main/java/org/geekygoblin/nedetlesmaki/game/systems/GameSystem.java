19fd0e5e171f08497e599914704c5266b82fd8b2
Pierre <pierre@marijon.fr>
2013-11-26 04:14:52 +0100

    Test for end of level run, just print in System.out need call trigger.

--- game/src/main/java/org/geekygoblin/nedetlesmaki/game/systems/GameSystem.java
+++ game/src/main/java/org/geekygoblin/nedetlesmaki/game/systems/GameSystem.java
@@ -31,7 +31,9 @@ import com.artemis.Entity;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.Mapper;
 import com.artemis.systems.VoidEntitySystem;
+import com.artemis.utils.ImmutableBag;
 
+import org.geekygoblin.nedetlesmaki.game.components.gamesystems.Color;
 import org.geekygoblin.nedetlesmaki.game.components.gamesystems.Pushable;
 import org.geekygoblin.nedetlesmaki.game.components.gamesystems.Pusher;
 import org.geekygoblin.nedetlesmaki.game.components.gamesystems.Position;
@@ -55,7 +57,8 @@ import org.geekygoblin.nedetlesmaki.game.Game;
 public class GameSystem extends VoidEntitySystem {
 
     private EntityIndexManager index;
-
+    private boolean run;
+    
     @Mapper
     ComponentMapper<Pushable> pushableMapper;
     @Mapper
@@ -67,6 +70,8 @@ public class GameSystem extends VoidEntitySystem {
     @Mapper
     ComponentMapper<Plate> plateMapper;
     @Mapper
+    ComponentMapper<Color> colorMapper;
+    @Mapper
     ComponentMapper<Boostable> boostMapper;
     @Mapper
     ComponentMapper<BlockOnPlate> blockOnPlateMapper;
@@ -76,13 +81,19 @@ public class GameSystem extends VoidEntitySystem {
     @Inject
     public GameSystem(EntityIndexManager index) {
         this.index = index;
+        this.run = false;
     }
 
     @Override
     protected void processSystem() {
+        if(this.run) {
+            this.endOfLevel();
+        }
     }
 
     public ArrayList<Mouvement> moveEntity(Entity e, Position dirP) {
+        this.run = true;
+        
         /*Check if move possible*/
         Position oldP = this.getPosition(e);
 
@@ -94,7 +105,7 @@ public class GameSystem extends VoidEntitySystem {
             if (this.getBoost(e) > i) {
                 e.getComponent(Pusher.class).setPusher(true);
             }
-            
+
             if (this.positionIsVoid(newP)) {
                 Square s = index.getSquare(newP.getX(), newP.getY());
                 if (this.testStopOnPlate(e, s)) {
@@ -103,16 +114,16 @@ public class GameSystem extends VoidEntitySystem {
                     if (this.getBoost(e) != -1) {
                         e.getComponent(Pusher.class).setPusher(false);
                     }
-                    
+
                     return mouv;
                 }
                 if (!this.testBlockedPlate(e, s)) {
-                        mouv.add(runValideMove(oldP, newP, e));
-                   }
+                    mouv.add(runValideMove(oldP, newP, e));
+                }
             } else {
                 if (this.isPusherEntity(e)) {
                     ArrayList<Entity> aNextE = index.getSquare(newP.getX(), newP.getY()).getWith(Pushable.class);
-                    if(!aNextE.isEmpty()) {
+                    if (!aNextE.isEmpty()) {
                         Entity nextE = aNextE.get(0);
                         if (this.isPushableEntity(nextE)) {
                             mouv.addAll(this.moveEntity(nextE, dirP));
@@ -124,7 +135,7 @@ public class GameSystem extends VoidEntitySystem {
                 if (this.getBoost(e) != -1) {
                     e.getComponent(Pusher.class).setPusher(false);
                 }
-                
+         
                 return mouv;
             }
         }
@@ -154,7 +165,7 @@ public class GameSystem extends VoidEntitySystem {
 
             return m;
         }
-        
+
         return null;
     }
 
@@ -284,20 +295,30 @@ public class GameSystem extends VoidEntitySystem {
 
         return -1;
     }
-    
-    public void printIndex() {
-        for (int i = 0; i != 15; i++) {
-            for (int j = 0; j != 15; j++) {
-                Square s = index.getSquare(i, j);
-                if (s != null) {
-                    ArrayList<Entity> array = s.getAll();
-                    for (Entity e : array) {
-                        System.out.printf("[%d, %d] : ", i, j);
-                        System.out.print(e);
-                        System.out.print("\n");
-                    }
+
+    private void endOfLevel() {
+        ImmutableBag<Entity> plateGroup = this.index.getAllPlate();
+
+        for (int i = 0; i != plateGroup.size(); i++) {
+            Entity platE = plateGroup.get(i);
+
+            Plate plate = this.plateMapper.getSafe(platE);
+            Position p = this.positionMapper.getSafe(platE);
+            Color colorPlate = this.colorMapper.getSafe(platE);
+
+            ArrayList<Entity> colorEntity = this.index.getSquare(p.getX(), p.getY()).getWith(Color.class);
+
+            if (colorEntity.size() == 2) {
+                Color colorA = this.colorMapper.getSafe(colorEntity.get(0));
+                Color colorB = this.colorMapper.getSafe(colorEntity.get(1));
+                if (colorA != colorB && !plate.isPlate()) {
+                    return;
                 }
+            } else {
+                return;
             }
         }
+
+        System.out.print("End of level\n");
     }
 }
