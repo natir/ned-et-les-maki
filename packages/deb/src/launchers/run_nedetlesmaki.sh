e64864c0ad5116af6484aeddfd73770ab7692792
devnewton <devnewton@bci.im>
2014-01-07 15:25:23 +0100

    gère la conf pourrie de debian stable avec Java 6 par défaut

--- packages/deb/src/launchers/run_nedetlesmaki.sh
+++ packages/deb/src/launchers/run_nedetlesmaki.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
-/opt/nedetlesmaki/nedetlesmaki-game-${project.version}.jar
+if [ -e /usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java ]
+then
+    /usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java -jar /opt/nedetlesmaki/nedetlesmaki-game-${project.version}.jar $*
+else
+    /opt/nedetlesmaki/nedetlesmaki-game-${project.version}.jar $*
+fi
