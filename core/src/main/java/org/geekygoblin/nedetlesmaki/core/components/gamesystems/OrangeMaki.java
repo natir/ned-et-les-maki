3f609ebfedebb505b04bd2f1c6f321fd4448582e
Pierre Marijon <pierre@marijon.fr>
2014-10-15 10:21:07 +0200

    Orange maki move and animation run.

--- core/src/main/java/org/geekygoblin/nedetlesmaki/core/components/gamesystems/OrangeMaki.java
+++ core/src/main/java/org/geekygoblin/nedetlesmaki/core/components/gamesystems/OrangeMaki.java
@@ -22,9 +22,17 @@
 package org.geekygoblin.nedetlesmaki.core.components.gamesystems;
 
 import com.artemis.Entity;
+import im.bci.jnuit.animation.IAnimationCollection;
+import im.bci.jnuit.animation.PlayMode;
+import im.bci.jnuit.artemis.sprite.Sprite;
+import im.bci.jnuit.artemis.sprite.SpritePuppetControls;
+import org.geekygoblin.nedetlesmaki.core.IAssets;
 import org.geekygoblin.nedetlesmaki.core.backend.LevelIndex;
+import org.geekygoblin.nedetlesmaki.core.backend.Position;
 import org.geekygoblin.nedetlesmaki.core.backend.PositionIndexed;
+import org.geekygoblin.nedetlesmaki.core.constants.AnimationTime;
 import org.geekygoblin.nedetlesmaki.core.constants.ColorType;
+import pythagoras.f.Vector3;
 
 /**
  *
@@ -32,41 +40,98 @@ import org.geekygoblin.nedetlesmaki.core.constants.ColorType;
  */
 public class OrangeMaki extends GameObject {
 
-    private boolean catchNed;
+    enum MoveType {
 
-    public OrangeMaki(PositionIndexed pos, Entity entity, LevelIndex index) {
+        NO,
+        VALIDATE,
+        UNVALIDATE,
+    }
+
+    private boolean validate;
+    private boolean catchNed;
+    private final IAnimationCollection animation;
+
+    public OrangeMaki(PositionIndexed pos, Entity entity, LevelIndex index, IAssets assets) {
         super(pos, entity, index);
         this.catchNed = false;
+        this.animation = assets.getAnimations("maki.json");
     }
 
     @Override
-    public ColorType getColorType() {
-        return ColorType.orange;
+    public Position moveTo(Position diff) {
+        Position n_pos = Position.sum(this.pos, diff);
+        GameObject n_obj = this.index.getGameObject(n_pos);
+        Plate c_plate = this.index.getPlate(this.pos);
+        Plate n_plate = this.index.getPlate(n_pos);
+
+        while (n_obj == null) { // while no objet move continue
+            this.pos.setPosition(n_pos);
+            if (n_plate != null && n_plate.getColorType() == ColorType.orange) { // Move to plate
+                if (this.validate) { // Actuali is in plate
+                    this.run_animation(diff, MoveType.NO);
+                    c_plate.setMaki(false);
+                } else {
+                    this.run_animation(diff, MoveType.VALIDATE);
+                    this.validate = true;
+                }
+                n_plate.setMaki(true);
+            } else { // Didn't move to plate
+                if (this.validate) { // Actuali is in plate
+                    this.run_animation(diff, MoveType.UNVALIDATE);
+                    this.validate = false;
+                    c_plate.setMaki(false);
+                } else {
+                    this.run_animation(diff, MoveType.NO);
+                }
+            }
+
+            n_pos = Position.sum(this.pos, diff);
+            n_obj = this.index.getGameObject(n_pos);
+            c_plate = this.index.getPlate(this.pos);
+            n_plate = this.index.getPlate(n_pos);
+        }
+
+        return this.pos;
+
     }
 
     @Override
-    public boolean isCatchNed() {
-        return true;
+    public void save(Memento m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean isPushable() {
-        return true;
+    public Memento undo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int getMovable() {
-        return 200;
+    private void run_animation(Position diff, MoveType type) {
+        Sprite sprite = this.entity.getComponent(Sprite.class);
+        SpritePuppetControls updatable = this.entity.getComponent(SpritePuppetControls.class);
+
+        if (updatable == null) {
+            updatable = new SpritePuppetControls(sprite);
+        }
+
+        if (type == MoveType.NO) {
+            updatable.moveToRelative(new Vector3(diff.getY(), diff.getX(), 0), AnimationTime.fly);
+        } else if (type == MoveType.VALIDATE) {
+            updatable.startAnimation(this.animation.getAnimationByName("maki_orange_one"), PlayMode.ONCE)
+                    .moveToRelative(new Vector3(diff.getY(), diff.getX(), 0), AnimationTime.fly);
+        } else if (type == MoveType.UNVALIDATE) {
+            updatable.startAnimation(this.animation.getAnimationByName("maki_orange_out"), PlayMode.ONCE)
+                    .moveToRelative(new Vector3(diff.getY(), diff.getX(), 0), AnimationTime.fly);
+        }
+
+        this.entity.addComponent(updatable);
+        this.entity.changedInWorld();
     }
 
-    @Override
-    public void nedCatched(boolean t) {
-        this.catchNed = t;
+    public boolean isValidate() {
+        return validate;
     }
 
-    @Override
-    public boolean nedIsCatched() {
-        return this.catchNed;
+    public void setValidate(boolean validate) {
+        this.validate = validate;
     }
-
 }
